03c87086668a98bcd74441abbbdb4d4f8149e1e5
Muhammad Ahfas An Nahri <ahfas.annahri@gmail.com>
2020-03-29 11:53:44 +0700

    update

--- tersync.sh
+++ tersync.sh
@@ -1,30 +1,37 @@
 #!/usr/bin/env bash
 
-readonly terfile_file="Terfile"
+readonly cmd="${0##*/}"
+readonly terfile_file="Terfile.ini"
+readonly status_file="/var/lib/tersync/status"
 
 function __usage_help() {
 	cat <<-EOF | less
-	tersync.sh
+	$cmd
 
 	USAGE: 
-	  tersync.sh sync -s /src/dir/logs/ -d /dst/dir/frontend/ --name "Frontend" --exclude "PATTERN"
-	  tersync.sh mkconfig
-	  tersync.sh terfile 
-	  tersync.sh unsync Frontend-Sync
+	  $cmd sync -s /src/dir/logs/ -d /dst/dir/frontend/ --name "Frontend" --exclude "PATTERN"
+	  $cmd sync -s=/src/dir/logs/ -d=/dst/dir/backend/ -n=Backend
+	  $cmd sync -nCoreSync -d/dst/dir/ -s/source/dir 
+
+	  $cmd mkconfig
+	  $cmd terfile 
+	  $cmd unsync Frontend-Sync
 		
-	COMMANDS
-  sync  [OPTIONS]
+	COMMANDS:
+  sync [-s <source>] [-d <dst>] [-n <name>] ...
       Start sync
-  unsync  [NAME]
-      Unsync a sync?
+  unsync [name]
+      Unsync a running sync
+  list
+      List running syncs
   mkconfig
       Create Terfile with unassigned parameters
   terfile
       Load parameters from Terfile in cwd
   help
-      Display this message.
+      Display this message
 
-	OPTIONS
+	OPTIONS:
   -s --source  [DIR]
       Source directory to be synced
   -d --destination  [DIR]
@@ -54,11 +61,16 @@ function get_value() {
   mandatory=${3:-0}
   value="$(sed -n "s/.*$key *= *\([^ ]*.*\)/\1/p" $terfile_file | tr -d ' ')"
   [[ $mandatory -eq 1 ]] && [[ -z "$value" ]] && msg_error "Tersync error. Empty parameter for key $key."
+  case "$value" in
+    "false") value=0;;
+    "true") value=1;;
+    *) value="$value";;
+  esac
   echo "$value"
 }
 
 function msg_error() {
-  echo "$@"
+  echo "$@" >&2
   exit 1
 }
 
@@ -69,66 +81,117 @@ function __mkconfig() {
     [[ $REPLY =~ ^[Yy]$ ]] && exit 0
 	}
 
-  cat<<-EOF > Terfile
-		; Source directory to be synced
-		source_dir = 
+  cat <<EOF > Terfile.ini
+; Source directory to be synced
+source_dir = 
 
-		; Destination directory or host
-		; - /path/to/dst/  <-- notice the '/' in the end
-		; - user@host:/path/to/dst/ <-- !! EXPERIMENTAL !!
-		destination = 
+; Destination directory or host. Examples: 
+;  /path/to/dst/             # notice the '/' in the end
+;  user@host:/path/to/dst/   # this one is experimental
+destination = 
 
-		; Specify sync name, this is mandatory
-		name = 
+; Specify sync name, this is mandatory
+name = 
 
-		; Run this sync as daemon, means it will create new systemd entry
-		; and can be managed via systemd
-		; Value [ true, false ]
-		daemon = false
+; Run this sync as daemon, means it will create new systemd entry
+; and can be managed via systemd
+; Value [ true, false ]
+daemon = false
 
-		; Enable file exclusion via pattern
-		; Value [ true, false ]
-		exclude = false
-		; Specify pattern to exclude files from syncronization
-		exclude_pattern =
+; Enable file exclusion via pattern
+; Value [ true, false ]
+exclude = false
+; Specify pattern to exclude files from syncronization
+exclude_pattern =
 
-		; Do incremental sync instead of entire file sync. Suitable for log files
-		append = false
+; Do incremental sync instead of entire file sync. Suitable for log files
+; Value [ true, false ]
+append = false
 
-		; Trigger sync on file modify
-		; Value [ true, false ]
-		modify = false
+; Trigger sync on file modify
+; Value [ true, false ]
+modify = false
 
-		; Delete extranous file from destination directory
-		; Value [ true, false ]
-		delete = false
-	EOF
+; Delete extranous file from destination directory
+; Value [ true, false ]
+delete = false
+EOF
 
   exit 0
 }
 
-function __terfile() {
+function __getPid() { ps aux | grep -v grep | grep -E "$1.*$2" | awk '{print $2}' || pgrep "$name"; }
+
+function __syncExists() { grep -q "$1" "$status_file"; }
+
+function __storeSync() { tee -a "$status_file" <<<"$name:x:$source_dir:$destination:${opt_daemon:-false}"; }
+
+function __storePid() { sed -i "/$name/ s/x/$1/" "$status_file"; }
+
+function __checkSync() {
+  if ! -f "$status_file"; then tee "$status_file" <<<"$cmd -- v${version:-0.0.1}"; fi
+  if __syncExists "$name"; then msg_error "Duplicate sync name. Please specify other name."; fi
+  if __syncExists "$source_dir" && __syncExists "$destination"; then msg_error "Duplicate source and destination pair. Exiting"; fi
+}
+
+function __startSync() {
+  # Initial rsync
+  rsync -a -z ${rsyncOpts[@]} "$source_dir" "$destination" 
+  while inotifywait -e $(tr ' ' ',' <<<"${inotifyOpts[@]}") "$source_dir"; do
+    rsync -a -z ${rsyncOpts[@]:-} ${rsyncFlags[@]:-} "$source_dir" "$destination"
+  done &
+  __storePid "$!"
+}
+
+function __checkProcess() {
+  local _pid=`__getPid`
+  [[ "$_pid" ]] && kill -9 $_pid > /dev/null 2>&1
+}
+
+function __parse_options() {
+  [[ $verbose -eq 1 ]] && rsyncOpts+=("-v")
+  [[ $opt_append -eq 1 ]] && rsyncOpts+=("--append")
+  [[ $opt_delete -eq 1 ]] && { 
+    inotifyOpts+=("delete")
+    rsyncOpts+=("--delete")
+  }
+  [[ $opt_modify -eq 1 ]] && inotifyOpts+=("modify")
+  [[ "$opt_exclude" ]] && {
+    inotifyFlags+=("--exclude $opt_exclude")
+    rsyncFlags+=("--exclude $opt_exclude")
+  }
+}
+
+__arg_source=0
+__arg_dest=0
+__arg_name=0
+__arg_exlude=0
+
+opt_append=0
+opt_daemon=0
+opt_delete=0
+opt_modify=0
+opt_exclude=""
+dry_run=0
+verbose=0
+inotifyOpts=("create")
+rsyncOpts=()
+
+function __parse_terfile() {
+  [[ "$2" = "--debug" ]] && debug=1
   source_dir="$(get_value source_dir 1)"
   destination="$(get_value destination 1)"
   name="$(get_value name 1)"
   daemon="$(get_value daemon)"
-  [[ "$(get_value exclude)" = "true" ]] && opt_exclude="$(get_value exclude_pattern)"
+  [[ "$(get_value exclude)" -eq 1 ]] && opt_exclude="$(get_value exclude_pattern)"
   opt_append="$(get_value append)"
   opt_modify="$(get_value modify)"
   opt_delete="$(get_value delete)"
-}
 
-function __unsync() {
-  name="${1:-}"
-  [[ -z "$name" ]] && msg_error "Please specify sync name to be stopped."
+  __parse_options
 }
 
 function __parse_arguments() {
-  __arg_source=0
-  __arg_dest=0
-  __arg_name=0
-  __arg_exlude=0
-
   while [ $# -gt 0 ]; do
     case "$1" in
       -s|--source)
@@ -216,32 +279,52 @@ function __parse_arguments() {
         shift
         ;;
       --append)			opt_append=1;	shift ;;
+      --daemon)     opt_daemon=1; shift ;;
       --delete)			opt_delete=1;	shift	;;
       --dry-run)		dry_run=1;		shift ;;
       --modify)			opt_modify=1; shift ;;
       -v|--verbose) verbose=1;		shift ;;
       -?|-h|--help) __usage_help				;;
+      --debug)      debug=1;            ;;
       *) msg_error "Unknown option: $1" ;;
     esac
   done
+  __parse_options
+}
+
+function __unsync() {
+  name="${1:-}"
+  [[ -z "$name" ]] && msg_error "Please specify sync name to be stopped."
 }
 
 function __debug_args() {
+  echo "Name        = $name"
   echo "Source      = $source_dir"
   echo "Destination = $destination"
-  echo "Name        = $name"
-  echo "Exlude      = $opt_exclude"
-  echo "Append      = $opt_append"
-  echo "Delete      = $opt_delete"
-  echo "Modify      = $opt_modify"
+  echo "OPT Daemon  = $opt_daemon"
+  echo "OPT Exlude  = $opt_exclude"
+  echo "OPT Append  = $opt_append"
+  echo "OPT Delete  = $opt_delete"
+  echo "OPT Modify  = $opt_modify"
+  echo " --- rsync -a -z ${rsyncOpts[@]:-} ${rsyncFlags[@]:-} $source_dir $destination" 
+  echo " --- inotifywait -e $(tr ' ' ',' <<<"${inotifyOpts[@]:-}") ${inotifyFlags[@]:-} $source_dir" 
+
+  exit
 }
+
+[[ $# -eq 0 ]] && __usage_help
 
 case "$1" in
   sync)	shift; __parse_arguments $@ ;;
-  unsync) shift; __unsync "$1" ;;
+  unsync) shift; __unsync "${1?Specify sync name to be unsynced.}" ;;
+  list) __list ;;
   mkconfig)	__mkconfig ;;
-  terfile) __terfile ;;
+  terfile) __parse_terfile $@;;
   help|-h|--help|-?) __usage_help ;; 
 esac
 
-__debug_args
+[[ $debug -eq 1 ]] && __debug_args
+
+__checkSync
+__storeSync
+__startSync
