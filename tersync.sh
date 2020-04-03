c843447fdb544e46110794f0ce4b7879caf137ac
Muhammad Ahfas An Nahri <ahfas.annahri@gmail.com>
2020-04-03 22:35:46 +0700

    it works?

--- tersync.sh
+++ tersync.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/env bash
 
+set -o nounset
+
 readonly cmd="${0##*/}"
-readonly terfile_file="Terfile.ini"
-readonly status_file="status"
-#readonly status_file="/var/lib/tersync/status"
+readonly config_file="config.ini"
+#readonly status_file="$HOME/.local/share/tersync/status"
+readonly status_file="/var/lib/tersync/status"
 readonly log_file="output.log"
 
-exec 3>> $log_file
+#exec 3>> $log_file
 
 __arg_source=0
 __arg_dest=0
 __arg_name=0
-__arg_exlude=0
+__arg_exclude=0
 
+name=
+source_dir=
+destination=
 opt_append=0
 opt_daemon=0
 opt_delete=0
 opt_modify=0
 opt_exclude=""
+debug=0
 dry_run=0
-verbose=0
+opt_verbose=0
 inotifyOpts=("create")
 __checkProcessyncOpts=()
 
@@ -33,24 +39,24 @@ function __usage_help {
 	  $cmd sync -nCoreSync -d/dst/dir/ -s/source/dir 
 
 	  $cmd mkconfig
-	  $cmd terfile 
+	  $cmd config 
 	  $cmd unsync Frontend-Sync
 		
 	COMMANDS:
   add [-s <source>] [-d <dst>] [-n <name>] ...
       Add a sync to sync entry
-  sync [name]
+  start [name]
       Start a specified sync
-  unsync [name]
+  remove [name]
       Remove a sync from sync list
   stop [name]
       Stop a running sync
-  list
+  ps
       List running syncs
   mkconfig
       Create Terfile with unassigned parameters
-  terfile
-      Load parameters from Terfile in cwd
+  config
+      Load parameters from $config_file in cwd
   help
       Display this message
 
@@ -69,9 +75,9 @@ function __usage_help {
       Trigger sync on any file modifications.
   --delete                  
       Deletes any extraneous file on destination directory.
-
   -v --verbose
       Self-explanatory.
+
   -h --help
       Display this info message.
 	EOF
@@ -95,7 +101,7 @@ function msg_success { echo "[v] $@" >&2; }
 function get_value {
   key="$1"
   mandatory=${3:-0}
-  value="$(sed -n "s/.*$key *= *\([^ ]*.*\)/\1/p" $terfile_file | tr -d ' ')"
+  value="$(sed -n "s/.*$key *= *\([^ ]*.*\)/\1/p" $config_file | tr -d ' ')"
   [[ $mandatory -eq 1 ]] && [[ -z "$value" ]] && msg_error "Tersync error. Empty parameter for key $key."
   case "$value" in
     "false") value=0;;
@@ -111,14 +117,19 @@ function get_valueJson {
   jq -M "map(select(.name == \"$search\")) | .[].$key" $status_file | sed 's/"//g'
 }
 
+function get_memberJson {
+  local search="$1"
+  jq -r -M "map(select(.name == \"$search\")) | ." $status_file
+}
+
 function __mkconfig {
-  [[ -f "Terfile" ]] && {
-    read -p "Terfile already exists. Overwrite? [Y]: " -n 1 -r
+  [[ -f "$config_file" ]] && {
+    read -p "$config_file already exists. Overwrite? [Y]: " -n 1 -r
     echo
     [[ $REPLY =~ ^[Yy]$ ]] && exit 0
 	}
 
-  cat <<EOF > Terfile.ini
+  cat <<EOF > $config_file
 ; Source directory to be synced
 source_dir = 
 
@@ -152,6 +163,9 @@ modify = false
 ; Delete extranous file from destination directory
 ; Value [ true, false ]
 delete = false
+
+; Verbose sync output
+verbose = false
 EOF
 
   exit 0
@@ -161,7 +175,9 @@ EOF
 
 function __getPid { get_valueJson pid; }
 
-function __syncExists { grep -q "$1" "$status_file"; }
+function __syncExists { jq -e ".[] .name | select(. == \"$1\")" $status_file > /dev/null; }
+
+#function __syncExists { grep -q "$1" "$status_file"; }
 
 function __syncRunning { if [[ `__getPid` ]]; then return 0; else return 1; fi; }
 
@@ -176,10 +192,24 @@ function __storeSync {
 		--arg om "$opt_modify" \
 		--arg od "$opt_delete" \
 		--arg da "$opt_daemon" \
-		--arg pd "" \ '[{name: $nm, pid: $pd, source_dir: $sd, destination: $dst, daemon: $da, exclude: $oe, append: $oa, modify: $om, delete: $od}]'
+		--arg vb "$opt_verbose" \
+		--arg pd "" \ '[{name: $nm, pid: $pd, source_dir: $sd, destination: $dst, daemon: $da, exclude: $oe, append: $oa, modify: $om, delete: $od, verbose: $vb}]'
 	)
 
-  jq ". += $json_string" $status_file | sponge $status_file
+  if jq ". += $json_string" $status_file | sponge $status_file; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+function __subtractSync {
+  local search="$1"
+  local jsonfile="input.tmp.json"
+  get_memberJson $search | tee $jsonfile > /dev/null
+  jq -M --argfile arg $jsonfile 'map(select(any(.; contains($arg[]))==false))' $status_file | sponge $status_file
+
+  trap "rm -f $jsonfile" EXIT
 }
 
 function __storePid { jq "map(if .name == \"$name\" then . + {"pid":\"$1\"} else . end)" $status_file | sponge $status_file; }
@@ -187,37 +217,74 @@ function __storePid { jq "map(if .name == \"$name\" then . + {"pid":\"$1\"} else
 function __removePid { __storePid ""; }
 
 function __checkSyncForDuplicates {
-  if [[ ! -f "$status_file" ]]; then tee "$status_file" <<<"[]" > /dev/null; fi
+  if [[ ! -s "$status_file" ]]; then 
+    mkdir -p "${status_file%/*}"
+    tee "$status_file" <<<"[]" > /dev/null 
+  fi
   #if __syncRunning; then msg_error "Sync name: $name is already running. Stop it first"; fi
-  if __syncExists "$name"; then msg_error "Sync name: $name already exists. Please specify other name."; fi
-  local sdir=`get_valueJson source_dir`
-  local ddir=`get_valueJson destination`
+  if __syncExists "$1"; then msg_error "Sync name: $1 already exists. Please specify other name."; fi
+  local sdir=`jq '.[] | .source_dir' $status_file | grep $source_dir`
+  local ddir=`jq '.[] | .destination' $status_file | grep $destination`
   if [[ $sdir ]] && [[ $ddir ]]; then msg_error "Duplicate source and destination pair. Exiting"; fi
 }
 
 function __syncAdd {
+  local name="$1"
+  [[ -z "$name" ]] && msg_error "Please specify sync name."
   # Check for dupes
-  __checkSyncForDuplicates
+  __checkSyncForDuplicates "$name"
   # Then store the detail to status_file
-  [[ $opt_daemon -eq 1 ]] && __createService
-  __storeSync
+  if [[ $opt_daemon -eq 1 ]]; then __createService; fi
+  __storeSync || msg_error "Unable to add $name. Unkown error"
 
   msg_success "$name has been added to sync list."
 }
 
-function __startSync {
+function __syncRemove {
+  local name=$1
+  [[ -z $name ]] && msg_error "Please specify sync name to be deleted from list."
+  local pid=$(__getPid)
+  # Check if exists
+  __syncExists $name || msg_error "$name doesn't exist in sync list. Nothing to remove"
+  
+  if [[ "$pid" ]]; then 
+    msg_info "$name is still running."
+    read -p "Kill it? [N]" -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then 
+      __syncStop $name; 
+    else
+      exit 0
+    fi
+  fi
+
+  __subtractSync $name || msg_error "Error occured."
+
+  msg_success "$name has been removed from sync list."
+}
+
+function __syncStart {
   # Initial rsync
-  rsync -a -z ${rsyncOpts[@]} "$source_dir" "$destination" 
+  function __rsync { 
+    if [[ $opt_exclude ]]; then
+      rsync -a -z ${rsyncOpts[@]:-} --exclude "$opt_exclude" $source_dir $destination >&3; 
+    else
+      rsync -a -z ${rsyncOpts[@]:-} $source_dir $destination >&3; 
+    fi
+  }
+  # function __rsync { rsync -avvvz --exclude "*.log" $source_dir $destination ;}
+
+  __rsync
   while inotifywait -qe $(tr ' ' ',' <<<"${inotifyOpts[@]}") "$source_dir" 2>&1 >&3; do
-    rsync -a -z ${rsyncOpts[@]:-} ${rsyncFlags[@]:-} "$source_dir" "$destination" >&3
+    __rsync
   done &
   __storePid "$!"
 }
 
-function __stopSync {
+function __syncStop {
   name="$1"
   if [[ -z $name ]]; then msg_error "Please specify sync name to be stopped."; fi
-  local force=0 flag="$2" pid=$(__getPid)
+  local force=0 flag="${2:-}" pid=$(__getPid)
   case "$flag" in
     -f) force=1 ;;
     *) : ;;
@@ -226,35 +293,52 @@ function __stopSync {
   if [[ $force -ne 1 ]]; then __syncExists "$name" || msg_error "$name doesn't exist in sync list. Please add it first."; fi
   if [[ -z $pid ]]; then msg_error "$name is not running."; fi
 
-  msg_info "About to terminate $pid"
+  msg_info "Terminating $name"
   if kill -9 $pid > /dev/null 2>&1; then
     __removePid
     msg_success "$name is successfully stopped."
   else
     msg_error "Unable to terminate process $name."
   fi
+}
 
-  exit 0
+function __syncList {
+  local all=0
+  case "${1:-}" in
+    -a|--all) all=1 ;;
+    *) :;;
+  esac
+
+  [[ ! -s $status_file ]] && msg_error "No entries."
+  local json_string=$(jq -r '.[] | "\(.name),\(.pid),\(.source_dir),\(.destination)" ' $status_file)
+  local header="Sync Name,PID,Source,Destination"
+  if [[ $all -eq 0 ]]; then
+    awk -F, 'BEGIN { OFS=FS }; {if ($2==""){$0=""} print}' <<<"$json_string" | column -t -s, -N "$header"
+  else
+    awk -F, 'BEGIN { OFS=FS }; {if ($2==""){$2="null"} print}' <<<"$json_string" | column -t -s, -N "$header"
+  fi
 }
 
 function __createService {
+  [[ $EUID -ne 0 ]] && msg_error "Please run as administrative user."
+  [[ -z $(whereis $cmd | awk '{print $2}') ]] && msg_error "$cmd is not installed correctly."
   local service_file="/etc/systemd/system/${name}.service"
   [[ -f $service_file ]] && msg_error "Service $name already exist."
-  
   cat<<EOF > $service_file
 [Unit]
 Description= $name sync daemon
 
 [Service]
 Type=oneshot
-ExecStart=$(realpath $0) sync $name
+ExecStart=$cmd start $name
 RemainAfterExit=true
-ExecStop=$(realpath $0) stop $name
+ExecStop=$cmd stop $name
 
 [Install]
 WantedBy=default.target
 EOF
-  msg_success "Service $name has been successfully created. You can now start it using systemctl"
+  systemctl daemon-reload
+  msg_success "Service $name has been successfully created."
 }
 
 function __verifyOptions {
@@ -266,25 +350,22 @@ function __verifyOptions {
     msg_error "Source path given [$source_dir] is not valid."
   fi
 
-  [[ ! -d "$destination" ]] && msg_error "Destination path given [$destination] is not valid."
+  [[ ! -d "$destination" ]] && msg_error "Destination path given is not valid: $destination"
 
-  [[ $verbose -eq 1 ]] && rsyncOpts+=("-v")
+  [[ $opt_verbose -eq 1 ]] && rsyncOpts+=("-v")
   [[ $opt_append -eq 1 ]] && rsyncOpts+=("--append")
   [[ $opt_delete -eq 1 ]] && { 
     inotifyOpts+=("delete")
     rsyncOpts+=("--delete")
   }
   [[ $opt_modify -eq 1 ]] && inotifyOpts+=("modify")
-  [[ "$opt_exclude" ]] && {
-    inotifyFlags+=("--exclude $opt_exclude")
-    rsyncFlags+=("--exclude $opt_exclude")
-  }
+  [[ "$opt_exclude" ]] && inotifyFlags+=("--exclude $opt_exclude")
 
   [[ $debug -eq 1 ]] && __debug_args
 }
 
-function __parse_terfile {
-  [[ "$2" = "--debug" ]] && debug=1
+function __parse_config {
+  [[ "${2:-}" = "--debug" ]] && debug=1
   source_dir="$(get_value source_dir 1)"
   destination="$(get_value destination 1)"
   name="$(get_value name 1)"
@@ -293,8 +374,9 @@ function __parse_terfile {
   opt_append="$(get_value append)"
   opt_modify="$(get_value modify)"
   opt_delete="$(get_value delete)"
+  opt_verbose="$(get_value verbose)"
 
-  __syncAdd
+  __syncAdd "$name"
 }
 
 function __parse_arguments {
@@ -304,7 +386,7 @@ function __parse_arguments {
       -s|--source)
         [[ $__arg_source -eq 1 ]] && exit 2 
         source_dir="${2:-}"
-        [[ -z "$souce_dir" ]] && msg_error "Please specify source dir."
+        [[ -z "$source_dir" ]] && msg_error "Please specify source dir."
         __arg_source=1
         shift 2
         ;;
@@ -367,22 +449,22 @@ function __parse_arguments {
       -e|--exclude)
         [[ $__arg_exclude -eq 1 ]] && exit 2
         opt_exclude="${2:-}"
-        [[ -z "$opt_exclude" ]] && msg_error "Please specify exlude pattern."
-        __arg_exlude=1
+        [[ -z "$opt_exclude" ]] && msg_error "Please specify exclude pattern."
+        __arg_exclude=1
         shift 2
         ;;
       -e=*)
         [[ $__arg_exclude -eq 1 ]] && exit 2
-        [[ -z "${1#*=}" ]] && msg_error "Please specify exlude pattern."
+        [[ -z "${1#*=}" ]] && msg_error "Please specify exclude pattern."
         opt_exclude="${1#*=}"
-        __arg_exlude=1
+        __arg_exclude=1
         shift
         ;;
       -e*)
         [[ $__arg_exclude -eq 1 ]] && exit 2
-        [[ -z "${1#-e}" ]] && msg_error "Please specify exlude pattern."
+        [[ -z "${1#-e}" ]] && msg_error "Please specify exclude pattern."
         opt_exclude="${1#-e}"
-        __arg_exlude=1
+        __arg_exclude=1
         shift
         ;;
       --append)			opt_append=1;	shift ;;
@@ -392,18 +474,17 @@ function __parse_arguments {
       --modify)			opt_modify=1; shift ;;
       -v|--verbose) verbose=1;		shift ;;
       -?|-h|--help) __usage_help				;;
-      --debug)      debug=1;            ;;
+      --debug)      debug=1;      shift ;;
       *) msg_error "Unknown option: $1" ;;
     esac
   done
-
-  __syncAdd
+  __syncAdd "$name"
 }
 
 function __sync {
   name="$1"
   if [[ -z $name ]]; then msg_error "Please specify sync name."; fi
-  if [[ $2 = "-d" ]]; then debug=1; fi
+  if [[ ${2:-} = "-d" ]]; then debug=1; fi
 
   __syncExists "$name" || msg_error "$name doesn't exist in sync list. Please add it first."
 
@@ -425,12 +506,7 @@ function __sync {
   fi
 
   # Execute
-  __startSync
-}
-
-function __unsync {
-  name="${1:-}"
-  [[ -z "$name" ]] && msg_error "Please specify sync name to be stopped."
+  __syncStart
 }
 
 function __debug_args {
@@ -452,17 +528,19 @@ function main {
   [[ $# -eq 0 ]] && __usage_help
   
   case "$1" in
-    add) shift; __parse_arguments $@ ;;
-    start)	shift; __sync $@ ;;
-    remove) shift; __unsync $@ ;;
-    stop) shift; __stopSync $@;;
-    status) shift; __syncStatus $@;;
-    list) shift; __list $@ ;;
-    mkconfig)	__mkconfig ;; 
-    terfile) __parse_terfile $@;;
+    a|add) shift; __parse_arguments $@ ;;
+    st|start)	shift; __sync $@ ;;
+    rm|remove) shift; __syncRemove $@ ;;
+    st|stop) shift; __syncStop $@;;
+    #stat*|status) shift; __syncStatus $@;;
+    ps|ls|list) shift; __syncList $@ ;;
+    mk|mkconfig)	__mkconfig ;; 
+    cf|config) __parse_config $@;;
     help|-h|--help|-?) __usage_help ;; 
   esac
   
 }
 
 main $@
+
+exit 0
